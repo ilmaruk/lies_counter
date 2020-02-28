a012f467d256a5cbf8e3c0ffa3bde091eeaa16ba
ilmaruk <ruggero.ferretti@gmail.com>
2020-02-28 21:27:13 +0000

    Initial rendering.

--- lies_counter/app.py
+++ lies_counter/app.py
@@ -7,9 +7,16 @@ from aiohttp import web
 
 
 async def home(request):
+    db = request.app["db"]
+    ebj = await _get_score(db, "ebj")
+    rgf = await _get_score(db, "rgf")
+    max_val = max(ebj, rgf)
+
     context = {
-        "ebj": 2,
-        "rgf": 2,
+        "ebj": ebj,
+        "ebj_ratio": int(ebj / max_val * 100),
+        "rgf": rgf,
+        "rgf_ratio": int(rgf / max_val * 100),
     }
     response = aiohttp_jinja2.render_template("index.html", request,
                                               context=context)
@@ -18,15 +25,20 @@ async def home(request):
 
 
 async def increment(request):
-    user_id = request.match_info.get('name', "Anonymous")
+    user_id = request.match_info.get("userid")
     await request.app["db"].execute("INCR", user_id)
-    return web.Response()
+    return web.Response(text=str(await _get_score(request.app["db"], user_id)))
 
 
 async def decrement(request: web.Request):
-    user_id = request.match_info.get('name', "Anonymous")
+    user_id = request.match_info.get("userid")
     await request.app["db"].execute("DECR", user_id)
-    return web.Response()
+    return web.Response(text=str(await _get_score(request.app["db"], user_id)))
+
+
+async def _get_score(db, key):
+    score = await db.get(key)
+    return int(score)
 
 
 async def connect_to_db(app):
@@ -37,8 +49,8 @@ def init():
     app = web.Application()
     app.add_routes([
         web.get('/', home),
-        web.post('/inc/{name}', increment),
-        web.post('/dec/{name}', decrement),
+        web.post('/inc/{userid}', increment),
+        web.post('/dec/{userid}', decrement),
     ])
 
     app.on_startup.append(connect_to_db)
